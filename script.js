7d02b9f4f02bc6a91a770e26f5df16441f634e76
jump4r <jump3r@dggaming.net>
2018-02-28 19:42:45 -0600

    add mobile compatibility

--- script.js
+++ script.js
@@ -9,61 +9,6 @@ document.addEventListener('DOMContentLoaded', _ => {
 });
 
 // Smooth Scrolling
-function currentYPosition() {
-    // Firefox, Chrome, Opera, Safari
-    if (self.pageYOffset) {
-        return self.pageYOffset;
-    }
-
-    // Internet Explorer 6
-    if (document.documentElement && document.documentElement.scrollTop) {
-        return document.documentElement.scrollTop;
-    }
-
-    // Internet Explorer 6, 7 and 8
-    if (document.body.scrollTop) {
-        return document.body.scrollTop;
-    } 
-
-    return 0;
-}
-
-function elementYPosition(eID) {
-    var e = document.getElementById(eID);
-    var y = e.offsetTop;
-    var node = e;
-    while (node.offsetParent && node.offsetParent != document.body) {
-        node = node.offsetParent;
-        y += node.offsetTop;
-    }
-
-    return y;
-}
-
-function smoothScroll(eID) {
-
-    var startY = currentYPosition();
-    var stopY = elementYPosition(eID);
-    var distance = stopY > startY ? stopY - startY : startY - stopY;
-    if (distance < 100) {
-        scrollTo(0, stopY); return;
-    }
-    var speed = Math.round(distance / 100);
-    if (speed >= 20) speed = 20;
-    var step = Math.round(distance / 25);
-    var leapY = stopY > startY ? startY + step : startY - step;
-    var timer = 0;
-    if (stopY > startY) {
-        for ( var i=startY; i<stopY; i+=step ) {
-            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
-        } return;
-    }
-    for ( var i=startY; i>stopY; i-=step ) {
-        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
-    }
-}
 
 function scroll(eid) {
     document.getElementById(eid).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
